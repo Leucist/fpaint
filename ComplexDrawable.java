015301c76d6c492667f9d2542e61a1c61dd368cc
leucistic <leucist@yandex.by>
2023-06-17 17:19:44 +0200

    Import ComplexDrawable

---
+++ ComplexDrawable.java
@@ -0,0 +1,54 @@
+import javafx.scene.Node;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import java.util.ArrayList;
+
+public abstract class ComplexDrawable extends Drawable {
+    private final ArrayList<Drawable> components;
+    ComplexDrawable(Color clr, ArrayList<Drawable> components) {
+        super(clr);
+        this.components = components;
+        setColor(clr);
+    }
+
+    public ArrayList<Drawable> getComponents() { return this.components; }
+
+    @Override
+    public void setColor(Color clr) {
+        super.setColor(clr);
+        for (Drawable component : this.components) {
+            component.setColor(clr);
+        }
+    }
+
+    @Override
+    public void setSelection(boolean b) {
+        super.setSelection(b);
+        for (Drawable component : this.components) {
+            component.setSelection(b);
+        }
+    }
+
+    @Override
+    public void draw(GraphicsContext g) {
+        for (Drawable component : this.components) {
+            component.draw(g);
+        }
+    }
+
+    @Override
+    public void move(double diffX, double diffY) {
+        for (Drawable obj : this.components) {
+            obj.move(diffX, diffY);
+        }
+    }
+
+    public boolean borderCollides(double x, double y) {
+        for (Drawable component : this.components) {
+            if (component.collides(x, y)) return true;
+        }
+        return false;
+    }
+}
